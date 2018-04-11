24fed243e159ca7b341a85bffffb2efaa3b4a327
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-11 18:29:04 +0200

    Adding some comments

--- Source/commands/Command.js
+++ Source/commands/Command.js
@@ -2,6 +2,10 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-export class Command {
+
+/**
+ * Defines the base of a command
+ */
+ export class Command {
     type='';
 }
