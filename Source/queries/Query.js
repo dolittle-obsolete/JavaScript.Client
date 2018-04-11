d52ba2c19beeeaf6456b2c0b867008b71fb45cdf
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-11 18:30:17 +0200

    Query base

---
+++ Source/queries/Query.js
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/**
+ * Defines the base of a query
+ */
+ export class Query {
+     nameOfQuery="";
+     generatedFrom="";
+     readModel=null;
+}
