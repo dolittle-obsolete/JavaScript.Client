ac13feee3ccd98679d0e736f247afd7c8f109f4a
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-24 22:04:41 +0200

    Changing to fetch API

--- Source/queries/QueryCoordinator.js
+++ Source/queries/QueryCoordinator.js
@@ -2,25 +2,27 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { HttpClient } from 'aurelia-http-client';
-import { inject } from 'aurelia-framework';
+import { Query } from './Query';
 import { QueryRequest } from './QueryRequest';
 
-@inject(HttpClient)
+/**
+ * Represents the coordinator of queries
+ */
 export class QueryCoordinator {
-
-    constructor(httpClient) {
-        this._httpClient = httpClient;
-    }
-
+    /**
+     * Execute a query
+     * @param {Query} query 
+     */
     execute(query) {
         let promise = new Promise((resolve, reject) => {
-            this._httpClient.createRequest('/api/Dolittle/Queries')
-            .asPost()
-            .withContent(QueryRequest.createFrom(query))
-            .send()
-            .then(result => {
-                let queryResult = JSON.parse(result.response);
+            fetch('/api/Dolittle/Queries', {
+                method: 'POST',
+                body: JSON.stringify(QueryRequest.createFrom(query)),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                let queryResult = response.json();
                 resolve(queryResult);
             });
         });
