5ba2f274efd85637af2de3bbb06286b64aaedd6d
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-09 12:52:04 +0100

    Fixing broken specs

--- Source/commands/for_CommandCoordinator/when_handling.js
+++ Source/commands/for_CommandCoordinator/when_handling.js
@@ -2,26 +2,34 @@ import { CommandCoordinator } from '../CommandCoordinator';
 
 describe('when handling', () => {
     let commandResult = {'something': 'result'};
-    let httpClient = {
-        createRequest: () => {
-            return {
-                asPost: sinon.stub(),
-                withContent: sinon.stub(),
-                send: (callback) => {
-                    callback(commandResult);
+    let requestUsed = null;
+    let fetchOptions = null;
+    global.fetch = (request, options) => {
+        requestUsed = request;
+        fetchOptions = options;
+        return {
+            then: (callback) => {
+                let result = callback({
+                    json: () => {
+                        return commandResult;
+                    }
+                });
+
+                return {
+                    then: (callback) => {
+                        callback(result);
+                    }
                 }
             }
         }
-    }
-    let commandCoordinator = new CommandCoordinator(httpClient);
+    };
+    let commandCoordinator = new CommandCoordinator();
     let command = {};
     let result = null;
-
 
     (beforeEach => {
         commandCoordinator.handle(command).then(r => result = r);
     })();
 
     it("should pass along the result", () => result.should.equal(commandResult));
-
 });
